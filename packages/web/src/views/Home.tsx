7c271f0f0a0c03659be13d7eef097462c91752ae
Benjamín Briones <benjaminbrionessorarrain@alu.frlp.utn.edu.ar>
2026-05-15 14:21:29 -0300

    chore(payment): agregar archivos faltantes del flujo de pagos

--- packages/web/src/views/Home.tsx
+++ packages/web/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { Box, SimpleGrid, Heading, Text, VStack } from "@chakra-ui/react";
-import { LuUsers } from "react-icons/lu";
+import { LuCreditCard, LuUsers } from "react-icons/lu";
 import { SectionCard } from "../components/SectionCard";
 
 export function HomeView() {
@@ -29,6 +29,13 @@ export function HomeView() {
           description="Administra el padrón de socios, sus categorías, estados de cuenta y datos personales."
           to="/members"
           icon={LuUsers}
+        />
+
+        <SectionCard
+          title="Pagos"
+          description="Registra cuotas y movimientos economicos asociados a socios existentes."
+          to="/payments"
+          icon={LuCreditCard}
         />
 
         {/* Future sections can be added here following the same pattern */}
