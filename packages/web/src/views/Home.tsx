9d5cd4161d6ecf4d6560e85f6364d9dc5556350c
geraltamirano22 <geraltamirano10@gmail.com>
2026-05-16 01:41:49 -0300

    feat(web): agrega tarjeta 'Deportes' en Home.tsx

--- packages/web/src/views/Home.tsx
+++ packages/web/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { Box, SimpleGrid, Heading, Text, VStack } from "@chakra-ui/react";
-import { LuUsers } from "react-icons/lu";
+import { LuUsers, LuDumbbell } from "react-icons/lu";
 import { SectionCard } from "../components/SectionCard";
 
 export function HomeView() {
@@ -29,6 +29,13 @@ export function HomeView() {
           description="Administra el padrón de socios, sus categorías, estados de cuenta y datos personales."
           to="/members"
           icon={LuUsers}
+        />
+        
+        <SectionCard 
+          title="Deportes"
+          description="Gestiona el catálogo de actividades, cupos disponibles, precios y disciplinas federadas."
+          to="/sports"
+          icon={LuDumbbell}
         />
 
         {/* Future sections can be added here following the same pattern */}
